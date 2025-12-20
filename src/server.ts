42326a2bd8565775e4e0ee11732b55347b7a8a50
Muhammad Nadhif Triyudo <mnadhiftriyudo@gmail.com>
2025-12-20 23:19:25 +0700

    Refactor server.ts to export web as default

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import { web } from "./app/web";
 // import { logger } from "./app/logging";
-import serverless from "serverless-http";
 
-export const handler = serverless(web);
-
+export default web;
 // web.listen(3000, () => {
 //     logger.info("Listening on port 3000")
 // })
