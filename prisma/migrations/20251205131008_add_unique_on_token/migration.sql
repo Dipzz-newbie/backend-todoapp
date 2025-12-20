cb4c73ba687f03253ba58406e4bb8a57caf7763a
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-12-20 20:08:37 +0700

    Add: new database migrate for postgresql

--- prisma/migrations/20251205131008_add_unique_on_token/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[token]` on the table `refresh_token` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `refresh_token_token_key` ON `refresh_token`(`token`);
