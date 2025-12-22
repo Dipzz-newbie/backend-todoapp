46cee5c60afc44325e7435496f723ee05694dd0c
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-12-22 21:35:57 +0700

    Update

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,38 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const web_1 = require("./app/web");
-// import serverless from "serverless-http"
+exports.web = void 0;
+const express_1 = __importDefault(require("express"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
+const public_api_1 = require("./route/public-api");
+const error_middleware_1 = require("./middleware/error-middleware");
+const api_1 = require("./route/api");
+const cors_1 = __importDefault(require("cors"));
+exports.web = (0, express_1.default)();
+const corsOptions = {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+const ratelimiter = (0, express_rate_limit_1.default)({
+    windowMs: 60 * 1000,
+    max: 100,
+    message: {
+        status: 429,
+        message: "Too many request"
+    }
+});
+exports.web.use((0, cors_1.default)(corsOptions));
+exports.web.use("/uploads", express_1.default.static("uploads"));
+exports.web.use(express_1.default.json());
+exports.web.use(ratelimiter);
+exports.web.use(public_api_1.publicRouter);
+exports.web.use(api_1.apiRouter);
+exports.web.use(error_middleware_1.errorMiddleware);
 const logging_1 = require("./app/logging");
 // export default serverless(web);
-web_1.web.listen(3000, () => {
+exports.web.listen(3000, () => {
     logging_1.logger.info("Listening on port 3000");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,35 @@
-import {web} from "./app/web"
-// import serverless from "serverless-http"
+import express from "express";
+import rateLimit from "express-rate-limit";
+import { publicRouter } from "./route/public-api";
+import { errorMiddleware } from "./middleware/error-middleware";
+import { apiRouter } from "./route/api";
+import cors from "cors"
+
+export const web = express();
+
+const corsOptions = {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+const ratelimiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 100,
+    message: {
+        status: 429,
+        message: "Too many request"
+    }
+})
+
+web.use(cors(corsOptions));
+web.use("/uploads", express.static("uploads"));
+web.use(express.json());
+web.use(ratelimiter);
+web.use(publicRouter);
+web.use(apiRouter);
+web.use(errorMiddleware);
+
 import { logger } from "./app/logging";
 
 // export default serverless(web);
