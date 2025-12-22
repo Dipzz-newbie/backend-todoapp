12b511960d8c944ed17075abbf3c77b60e83560c
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-12-22 20:34:00 +0700

    Update

--- api/server.ts
+++
@@ -1,8 +0,0 @@
-import {web} from "../src/app/web"
-import serverless from "serverless-http"
-// import { logger } from "./app/logging";
-
-export default serverless(web);
-// web.listen(3000, () => {
-//     logger.info("Listening on port 3000")
-// })
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import {web} from "./app/web"
+// import serverless from "serverless-http"
+import { logger } from "./app/logging";
+
+// export default serverless(web);
+web.listen(3000, () => {
+    logger.info("Listening on port 3000")
+})
